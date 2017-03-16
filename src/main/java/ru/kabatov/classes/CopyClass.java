cba3d1dc8b93280c7a11d1cded0359c2580b6bb7
Кабатов <zxvz>
2017-03-16 09:15:19 +0300

    Upgrade

--- src/main/java/ru/kabatov/classes/CopyClass.java
+++ src/main/java/ru/kabatov/classes/CopyClass.java
@@ -9,10 +9,11 @@ import java.nio.file.Files;
  */
 public class CopyClass {
     public static void main(String[] args) {
-        String[] fileName = new String[3];
+        String[] fileName = new String[4];
         fileName[0]="HFBook.txt";
         fileName[1] = "Internet.txt";
         fileName[2] = "Marduk.txt";
+        fileName[3] = "DsO.txt";
         String to = System.getProperty("user.dir")+"/src/main/webapp/WEB-INF/lib/";
         String from = System.getProperty("user.dir")+"/target/TranslateServlet-1.0-SNAPSHOT/WEB-INF/lib/";
         for (String k : fileName){
